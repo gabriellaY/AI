6e11edc0bcc43251f601b6f28cb578966c20073a
gabriellaY <g.yoncheva1@gmail.com>
2019-11-10 16:07:31 +0200

    Refactoring

--- NQueens/NQueensProblem/NQueensProblem.cs
+++ NQueens/NQueensProblem/NQueensProblem.cs
@@ -9,170 +9,26 @@ namespace NQueensProblem
     public class NQueensProblem
     {
         private readonly int _boardSize;
-        private readonly int diagonalSize;
+        private readonly int _diagonalSize;
         private readonly Random _randomIndex;
         private readonly int[] _queens; //queens' positions
 
-        private int[] _conflictsInColumns;
-        private int[] _conflictsInD1; //primary diagonal
-        private int[] _conflictsInD2; //secondary diagonal
+        private int[] _conflictsInColumns; //number of queens in column
+        private int[] _conflictsInD1; //number of queens in primary diagonal
+        private int[] _conflictsInD2; //number of queens in secondary diagonal
 
         public NQueensProblem(int N)
         {
             this._boardSize = N;
-            diagonalSize = 2 * _boardSize - 1;
+            _diagonalSize = 2 * _boardSize - 1;
             _queens = new int[_boardSize];
             _conflictsInColumns = new int[_boardSize];
-            _conflictsInD1 = new int[diagonalSize];
-            _conflictsInD2 = new int[diagonalSize];
+            _conflictsInD1 = new int[_diagonalSize];
+            _conflictsInD2 = new int[_diagonalSize];
             _randomIndex = new Random();
 
             GenerateQueensPositions();
             TrackConflicts();
-        }
-
-
-        public string GetSolution(int rows)
-        {
-            StringBuilder solutionBoard = new StringBuilder();
-
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < rows; col++)
-                {
-                    if (_queens[row] == col)
-                    {
-                        solutionBoard.Append(col == rows - 1 ? "*\n" : "* ");
-                    }
-                    else
-                    {
-                        solutionBoard.Append(col == rows - 1 ? "_\n" : "_ ");
-                    }
-                }
-            }
-
-            return solutionBoard.ToString();
-        }
-
-        private int GetIndexInD1(int row, int column)
-        {
-            int indexInD1;
-
-            if (row <= column)
-            {
-                indexInD1 = _boardSize - 1 - Math.Abs(row - column);
-            }
-            else
-            {
-                indexInD1 = _boardSize - 1 + Math.Abs(row - column);
-            }
-
-            return indexInD1;
-        }
-
-        //get row with min conflicts
-        private int GetQueenWithMinConflicts(int row)
-        {
-            List<int> minConflictsQueens = new List<int>();
-
-            int minConflicts = _boardSize;
-
-            for (int column = 0; column < _boardSize; column++)
-            {
-                if (column == _queens[row])
-                {
-                    continue;
-                }
-
-                int numberOfConflicts = CalculateConflictsWithQueens(row, column);
-
-                if (numberOfConflicts < minConflicts)
-                {
-                    minConflicts = numberOfConflicts;
-                    minConflictsQueens.Clear();
-                }
-                if (numberOfConflicts == minConflicts)
-                {
-                    minConflictsQueens.Add(column);
-                }
-            }
-
-            return minConflictsQueens[_randomIndex.Next(0, minConflictsQueens.Count)];
-        }
-
-        //get column with max conflicts
-        private int GetQueenWithMaxConflicts()
-        {
-            List<int> maxConflictsQueens = new List<int>();
-
-            int maxConflicts = 0;
-
-            for (int i = 0; i < _boardSize; i++)
-            {
-                int numberOfConflicts = CalculateConflictsWithQueens(i, _queens[i]);
-
-                if (numberOfConflicts > maxConflicts)
-                {
-                    maxConflicts = numberOfConflicts;
-                    maxConflictsQueens.Clear();
-                }
-                if (numberOfConflicts == maxConflicts)
-                {
-                    maxConflictsQueens.Add(i);
-                }
-            }
-
-            if (maxConflicts == 0)
-            {
-                return -1;
-            }
-            else
-            {
-                return maxConflictsQueens[_randomIndex.Next(0, maxConflictsQueens.Count)];
-            }
-        }
-
-        private int CalculateConflictsWithQueens(int row, int column)
-        {
-            int indexInD1 = GetIndexInD1(row, column);
-
-            if (_queens[row] == column)
-            {
-                return _conflictsInD1[indexInD1] + _conflictsInD2[Math.Abs(row + column)] + _conflictsInColumns[column] - 3;
-            }
-            else
-            {
-                return _conflictsInD1[indexInD1] + _conflictsInD2[Math.Abs(row + column)] + _conflictsInColumns[column];
-            }
-        }
-
-        private void Restart()
-        {
-            GenerateQueensPositions();
-
-            _conflictsInColumns = new int[_boardSize];
-            _conflictsInD1 = new int[diagonalSize];
-            _conflictsInD2 = new int[diagonalSize];
-
-            TrackConflicts();
-        }
-
-        private void TrackConflicts()
-        {
-            for (int i = 0; i < _boardSize; i++)
-            {
-                _conflictsInColumns[_queens[i]]++;
-                _conflictsInD1[(i - _queens[i]) + _boardSize - 1]++;
-                _conflictsInD2[i + _queens[i]]++;
-            }
-        }
-
-        private void GenerateQueensPositions()
-        {
-            for (int i = 0; i < _boardSize; i++)
-            {
-                _queens[i] = _randomIndex.Next(0, _boardSize);
-            }
         }
 
         public void SolveProblem()
@@ -230,5 +86,146 @@ namespace NQueensProblem
                 move++;
             }
         }
+
+        public string GetSolution(int rows)
+        {
+            StringBuilder solutionBoard = new StringBuilder();
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < rows; col++)
+                {
+                    if (_queens[row] == col)
+                    {
+                        solutionBoard.Append(col == rows - 1 ? "*\n" : "* ");
+                    }
+                    else
+                    {
+                        solutionBoard.Append(col == rows - 1 ? "_\n" : "_ ");
+                    }
+                }
+            }
+
+            return solutionBoard.ToString();
+        }
+
+        private int GetIndexInD1(int row, int column)
+        {
+            int indexInD1;
+
+            if (row <= column)
+            {
+                indexInD1 = _boardSize - 1 - Math.Abs(row - column);
+            }
+            else
+            {
+                indexInD1 = _boardSize - 1 + Math.Abs(row - column);
+            }
+
+            return indexInD1;
+        }
+
+        private int GetQueenWithMinConflicts(int row)
+        {
+            List<int> minConflictsQueens = new List<int>();
+
+            int minConflicts = _boardSize;
+
+            for (int column = 0; column < _boardSize; column++)
+            {
+                if (column == _queens[row])
+                {
+                    continue;
+                }
+
+                int numberOfConflicts = CalculateConflictsWithQueens(row, column);
+
+                if (numberOfConflicts < minConflicts)
+                {
+                    minConflicts = numberOfConflicts;
+                    minConflictsQueens.Clear();
+                }
+                if (numberOfConflicts == minConflicts)
+                {
+                    minConflictsQueens.Add(column);
+                }
+            }
+
+            return minConflictsQueens[_randomIndex.Next(0, minConflictsQueens.Count)];
+        }
+
+        private int GetQueenWithMaxConflicts()
+        {
+            List<int> maxConflictsQueens = new List<int>();
+
+            int maxConflicts = 0;
+
+            for (int i = 0; i < _boardSize; i++)
+            {
+                int numberOfConflicts = CalculateConflictsWithQueens(i, _queens[i]);
+
+                if (numberOfConflicts > maxConflicts)
+                {
+                    maxConflicts = numberOfConflicts;
+                    maxConflictsQueens.Clear();
+                }
+                if (numberOfConflicts == maxConflicts)
+                {
+                    maxConflictsQueens.Add(i);
+                }
+            }
+
+            if (maxConflicts == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return maxConflictsQueens[_randomIndex.Next(0, maxConflictsQueens.Count)];
+            }
+        }
+
+        private int CalculateConflictsWithQueens(int row, int column)
+        {
+            int indexInD1 = GetIndexInD1(row, column);
+
+            if (_queens[row] == column)
+            {
+                return _conflictsInD1[indexInD1] + _conflictsInD2[Math.Abs(row + column)] + _conflictsInColumns[column] - 3;
+            }
+            else
+            {
+                return _conflictsInD1[indexInD1] + _conflictsInD2[Math.Abs(row + column)] + _conflictsInColumns[column];
+            }
+        }
+
+        private void Restart()
+        {
+            GenerateQueensPositions();
+
+            _conflictsInColumns = new int[_boardSize];
+            _conflictsInD1 = new int[_diagonalSize];
+            _conflictsInD2 = new int[_diagonalSize];
+
+            TrackConflicts();
+        }
+
+        private void TrackConflicts()
+        {
+            for (int i = 0; i < _boardSize; i++)
+            {
+                _conflictsInColumns[_queens[i]]++;
+                _conflictsInD1[(i - _queens[i]) + _boardSize - 1]++;
+                _conflictsInD2[i + _queens[i]]++;
+            }
+        }
+
+        private void GenerateQueensPositions()
+        {
+            for (int i = 0; i < _boardSize; i++)
+            {
+                _queens[i] = _randomIndex.Next(0, _boardSize);
+            }
+        }       
     }
 }
